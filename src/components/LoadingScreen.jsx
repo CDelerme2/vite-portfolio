812221759ea92929f3efa5a2b18dc097f2dd96d0
CDelerme2 <delerme.biz@outlook.com>
2025-03-20 15:37:49 -0400

    Update LoadingScreen.jsx
    tweak width

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -28,7 +28,7 @@ export const LoadingScreen = ({ onComplete }) => {
         <span className="animate-blink ml-1">|</span>
       </div>
 
-      <div className="w-[950px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
+      <div className="w-[1100px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
         <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div>
       </div>
     </div>

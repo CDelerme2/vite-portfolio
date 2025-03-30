54abd479269e4c96c421e4bde913d8b6c88f4cb5
CDelerme2 <delerme.biz@outlook.com>
2025-03-30 14:29:42 -0400

    commit tweaks via cmd line

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 
 export const LoadingScreen = ({ onComplete }) => {
   const [message, setMessage] = useState("");
-  const welcomeText = "Welcome to Carlos Delerme's Professional Portfolio...";
+  const welcomeText =
+    "Welcome to Carlos Delerme's Web Development Portfolio...";
 
   useEffect(() => {
     let index = 0;
@@ -28,7 +29,7 @@ export const LoadingScreen = ({ onComplete }) => {
         <span className="animate-blink ml-1">|</span>
       </div>
 
-      <div className="w-[1100px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
+      <div className="w-[1150px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
         <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div>
       </div>
     </div>
